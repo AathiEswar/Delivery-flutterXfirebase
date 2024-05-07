5283ac490f80c0f4850574dc96474d7eae8e5701
AathiEswar <aathi28082003@gmail.com>
2024-05-07 21:41:26 +0530

    sliver app bar

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      home: LoginOrRegister(),
+      home: HomePage(),
       theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterxfirebase/components/custom_drawer.dart';
+import 'package:flutterxfirebase/components/custom_silver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,16 +13,31 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-          iconTheme: IconThemeData(color: Theme.of(context).colorScheme.inversePrimary),
-          title: Text("Home" ,style: TextStyle(
-          color: Theme.of(context).colorScheme.inversePrimary,
-
-        ),),
-          // backgroundColor: Theme.of(context).colorScheme.background
-      ),
+      // appBar: AppBar(
+      //
+      //     iconTheme: IconThemeData(color: Theme.of(context).colorScheme.inversePrimary),
+      //     title: Text("Home" ,style: TextStyle(
+      //     color: Theme.of(context).colorScheme.inversePrimary,
+      //
+      //   ),),
+      //     backgroundColor: Theme.of(context).colorScheme.background
+      // ),
       drawer : MyDrawer(),
+      body:   NestedScrollView(
+        headerSliverBuilder: (context , innerBoxIsScrolled) => [
+          MySliverAppBar(
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("HELLOOOOOOO")
+              ],
+            ),
+            title: Text("title"),
+          )
+        ],
+        body: Container(color: Colors.blue,),
+      ),
+
     );
   }
 }
